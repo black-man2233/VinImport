9105fba685a1f2d43bf58e5a4f236a48f6749637
Kevin Home -Pc <kevinbamwesa03@gmail.com>
2023-02-04 20:40:48 +0100

    Started with GUI, almost finished the home page

---
+++ VinImport(GUI)/ViewModel/HomeVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinImport_GUI_.Model.DateAndTime;
+
+namespace VinImport_GUI_.ViewModel
+{
+    internal class HomeVM:Utilities.ViewModelBase
+    {
+		private DateModel _date;
+		public DateModel Date
+		{
+			get { return _date; }
+			set { _date = value; OnPropertyChanged(); }
+		}
+
+		private string _userName;
+
+		public string UserName
+		{
+			get { return _userName; }
+			set { _userName = value;OnPropertyChanged(); }
+		}
+
+
+
+		public HomeVM()
+		{
+			_date = new DateModel();
+			_userName = "Kevin Bamwesa";
+
+		}
+
+	}
+}
---
+++ VinImport(GUI)/ViewModel/NavigationVM.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VinImport_GUI_.Utilities;
+
+namespace VinImport_GUI_.ViewModel
+{
+    internal class NavigationVM : Utilities.ViewModelBase
+    {
+        #region Properties
+        private object _currentView = new HomeVM();
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        private static string _totalCartItems = "0";
+        public string TotalCartItems
+        {
+            get
+            {
+                return _totalCartItems;
+            }
+            set
+            {
+                _totalCartItems = value;
+                OnPropertyChanged("TotalCartItems");
+            }
+        }
+        #endregion
+
+        #region ICommands
+        public ICommand HomeCommand { get; set; }
+        public ICommand ProductsCommand { get; set; }
+
+        #endregion
+
+        #region CommandsFunction
+        private void Home(Object obj) => CurrentView = new HomeVM();
+        private void Product(Object obj) => CurrentView = new ProductsVM();
+
+        #endregion
+
+        #region Constructor
+        public NavigationVM()
+        {
+            HomeCommand = new RelayCommand(Home);
+            ProductsCommand = new RelayCommand(Product);
+
+
+            //Startup Page
+            this.CurrentView = new HomeVM();
+        }
+        #endregion
+    }
+}
